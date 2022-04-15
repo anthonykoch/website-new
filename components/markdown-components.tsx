bc6e78ea38687e3fbab8a310d366e094e5b98134
Anthony Koch <goaway@github.com>
2022-04-15 00:20:28 -0700

    Optimize images

--- components/markdown-components.tsx
+++ components/markdown-components.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import cx from 'classnames'
-// import { useRouter } from 'next/router'
 import { TabbedImageCompare } from './shared/TabbedImageCompare'
-// import Link from 'next/link'
 
 const Anchor: React.FC<any> = (props) => {
   let p: Record<string, any> = {} as any
-  // const router = useRouter()
 
   if (props.href) {
     try {
@@ -17,7 +14,7 @@ const Anchor: React.FC<any> = (props) => {
         p = { ...p, target: '_blank', rel: 'noreferrer noopener ' }
       }
     } catch (err) {
-      //
+      // that's right, I'm breaking the rules. SWALLOW IT UP
     }
   }
 
