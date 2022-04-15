ffa9faaf874db8d1aa308be8120e4da47c3c4bcd
Anthony Koch <goaway@github.com>
2022-04-15 00:38:17 -0700

    Add sentry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,36 +25,6 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <HeadMeta />
-
-      {/* <Head>
-
-      </Head> */}
-      {/* <Script
-  dangerouslySetInnerHTML={{
-    __html: `
-// Make sure codepen doesn't load before nuxt does
-setTimeout(function () {
-  const script = document.createElement('script');
-
-  script.src = 'https://static.codepen.io/assets/embed/ei.js';
-  script.async = true;
-  script.defer = true;
-
-  document.body.appendChild(script);
-}, 1000)
-`,
-  }}
-></Script> */}
-      {/* <Script src="https://browser.sentry-cdn.com/4.6.1/bundle.min.js" crossorigin="anonymous"></Script> */}
-      {/* <script>
-  var isStaging = {{ JSON.stringify(ENV.app.isStaging) }}
-  var isEnvProduction = {{ JSON.stringify(ENV.app.isEnvProduction) }}
-
-  if (isEnvProduction && !isStaging) {
-    Sentry.init({ dsn: 'https://1049260bcb57442ebafa6890e89d1e5c@sentry.io/1395424' });
-  }
-</script> */}
-
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
