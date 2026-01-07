362032177e890ae2cb7a827e1d6127b5a12feaf4
Anthony Koch <anthonykoch@users.noreply.github.com>
2026-01-06 22:31:25 -0700

    Update title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ import { NavigationScrollReveal } from '@/features/site/Navigation/NavigationScr
 const HeadMeta = () => {
   return (
     <Head>
-      <title>website</title>
+      <title>Anthony Koch</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta
