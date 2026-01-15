52cbef91aeedf086849b9818d72ee980ffd4e051
Anthony Koch <anthonykoch@users.noreply.github.com>
2026-01-15 16:33:02 -0700

    stash

--- next.config.ts
+++ next.config.ts
@@ -15,42 +15,14 @@ const nextConfig: NextConfig = {
   //   ignoreDuringBuilds: true,
   // },
   turbopack: {
-    rules: {
-      // '*.svg': {
-      //   loaders: [
-      //     {
-      //       loader: '@svgr/webpack',
-      //       options: {
-      //         prettier: false,
-      //         svgo: true,
-      //         typescript: true,
-      //         svgoConfig: {
-      //           plugins: [
-      //             {
-      //               name: 'removeViewBox',
-      //               active: false,
-      //             },
-      //           ],
-      //         },
-      //         titleProp: true,
-      //       },
-      //     },
-      //   ],
-      //   as: '*.js',
-      // },
-    },
+    rules: {},
     resolveAlias: {
       '@': __dirname,
     },
   },
 }
 
-// const withMDX = createMDX({
-//   extension: /\.(md|mdx)$/,
-// })
-
 export default nextConfig
-// export default withMDX(nextConfig)
 
 // // eslint-disable-next-line
 // const { withSentryConfig } = require('@sentry/nextjs')
@@ -66,58 +38,6 @@ export default nextConfig
 //   silent: true, // Suppresses all logs
 //   // For all available options, see:
 //   // https://github.com/getsentry/sentry-webpack-plugin#options.
-// }
-
-// /* @type {import('next').NextConfig} */
-// const config = {
-//   resolveAlias: {
-//     '@': __dirname,
-//   },
-
-//   // reactStrictMode: true,
-//   // webpack(config, { dev, isServer }) {
-//   //   Object.assign(config.resolve.alias, {
-//   //     '@/': __dirname,
-//   //   })
-
-//   //   config.module.rules.push({
-//   //     test: /\.svg$/i,
-//   //     issuer: /\.[jt]sx?$/,
-//   //     use: [
-//   //       {
-//   //         loader: '@svgr/webpack',
-//   //         options: {
-//   //           prettier: false,
-//   //           svgo: true,
-//   //           typescript: true,
-//   //           svgoConfig: {
-//   //             plugins: [
-//   //               {
-//   //                 name: 'removeViewBox',
-//   //                 active: false,
-//   //               },
-//   //             ],
-//   //           },
-//   //           titleProp: true,
-//   //         },
-//   //       },
-//   //       {
-//   //         loader: path.join(
-//   //           __dirname,
-//   //           'node_modules/next/dist/build/webpack/loaders/next-image-loader.js',
-//   //         ),
-//   //         options: {
-//   //           isServer: isServer,
-//   //           isDev: dev,
-//   //           basePath: '',
-//   //           assetPrefix: '',
-//   //         },
-//   //       },
-//   //     ],
-//   //   })
-
-//   //   return config
-//   // },
 // }
 
 // const withBundleAnalyzer = require('@next/bundle-analyzer')({
