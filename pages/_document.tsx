40fd76c8cfef5f7906b558771f005f50d3c59a93
Anthony Koch <hello@anthonykoch.com>
2026-01-19 14:47:15 -0700

    Add dm sans locally

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,16 +6,6 @@ class AppDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" type="image/x-icon" href="/images/favicon.png" />
-          {/* <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/github.min.css"
-          /> */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
