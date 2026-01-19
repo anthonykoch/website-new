6630c50852f5793867dbd0d83c634e7d734222aa
Anthony Koch <hello@anthonykoch.com>
2026-01-18 19:32:35 -0700

    Make it static

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { withSentryConfig } from '@sentry/nextjs'
 import type { NextConfig } from 'next'
 
 let nextConfig: NextConfig = {
+  output: 'export',
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
